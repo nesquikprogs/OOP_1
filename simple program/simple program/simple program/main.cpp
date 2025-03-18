44f905f8b1626d885b793ad91211ebcf9d93c6eb
nesquikprogs <nevsquik.2301@mail.ru>
2025-03-18 23:18:28 +0900

    creating a module and header file

---
+++ simple program/simple program/simple program/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <clocale>
+#include <iomanip> 
+#include "triangle.h" // Подключаем заголовочный файл
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    double hypotenuse, leg1, leg2, radius;
+
+    cout << "Hypotenuse: ";
+    cin >> hypotenuse;
+    cout << "Leg 1: ";
+    cin >> leg1;
+
+    if (hypotenuse <= 0 || leg1 <= 0) {
+        cout << "Error! Side lengths must be positive." << endl;
+        return 1;
+    }
+    if (leg1 >= hypotenuse) {
+        cout << "Error! Leg can't be >= hypotenuse." << endl;
+        return 1;
+    }
+
+    // Вычисляем второй катет и радиус с помощью функций
+    leg2 = calculateLeg(hypotenuse, leg1);
+    radius = calculateRadius(hypotenuse, leg1, leg2);
+
+    cout << fixed << setprecision(3);
+    cout << "Leg 2: " << leg2 << endl;
+    cout << "Radius: " << radius << endl;
+
+    return 0;
+}
---
+++ simple program/simple program/simple program/triangle.h
@@ -0,0 +1,10 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+// Функция для вычисления второго катета
+double calculateLeg(double hypotenuse, double leg1);
+
+// Функция для вычисления радиуса вписанной окружности
+double calculateRadius(double hypotenuse, double leg1, double leg2);
+
+#endif // TRIANGLE_H
