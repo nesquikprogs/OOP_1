2a802d52fa6507c044c3a8d1a4357a6ff4a9419a
nesquikprogs <nevsquik.2301@mail.ru>
2025-03-18 23:45:02 +0900

    compiler settings

---
+++ simple program/main/main/triangle.h
@@ -0,0 +1,10 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+// ������� ��� ���������� ������� ������
+double calculateLeg(double hypotenuse, double leg1);
+
+// ������� ��� ���������� ������� ��������� ����������авава
+double calculateRadius(double hypotenuse, double leg1, double leg2);
+
+#endif // TRIANGLE_H
